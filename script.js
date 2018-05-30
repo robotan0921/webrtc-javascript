7a5ba8b65290d4b97de5e186e6ace1cc4af29e43
Takuro Yamamoto <takuro.1.yamamoto@sony.com>
2018-05-30 15:58:07 +0900

    Update part of google map

--- script.js
+++ script.js
@@ -69,14 +69,14 @@ peer.on('call', function(call) {
 
     initMap();
     $('#debug').text(call.metadata);
-    // theirLocation = call.metadata.match(/\d{1,}.\d{1,}/g);
-    // var container = document.getElementById("debug");
-    // var item = document.createElement("li");
-    // item.textContent = theirLocation[0];
-    // container.appendChild(item);
-    // var item2 = document.createElement("li");
-    // item2.textContent = theirLocation[1];
-    // container.appendChild(item2);
+    theirLocation = call.metadata.match(/\d{1,}.\d{1,}/g);
+    var container = document.getElementById("debug");
+    var item = document.createElement("li");
+    item.textContent = theirLocation[0];
+    container.appendChild(item);
+    var item2 = document.createElement("li");
+    item2.textContent = theirLocation[1];
+    container.appendChild(item2);
 });
 
 function setupCallEventHandlers(call){
